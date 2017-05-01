75f4d20230d397541fef889b51eb7a746fdc6e88
jpax1996 <duke.jp.freitas@gmail.com>
2017-05-01 11:38:26 +0200

    01.05 v2

--- test.py
+++ test.py
@@ -3,24 +3,24 @@ import matrix_module as mm
 
 class test_mat(unittest.TestCase):
 
-	def setUp(self):
+        def setUp(self):
 	
-		self.mat = mm.Matrix([1,1,1],[1,2,3])
+                self.mat = mm.Matrix([1,1,1],[1,2,3])
+
+        def test_add_mat(self):
 	
-	def test_add_mat(self):
-	
-		res = [2,2,2][2,4,6]
-		self.assertEqual(res, self.mm.Matrix.addition(self.mat,self.mat))
+                res = [[2,2,2],[2,4,6]]
+                self.assertEqual(res, mm.Matrix.addition(self.mat,self.mat,self.mat))
 
         def test_mult_mat(self):
 	
-		res = [1,1,1][1,4,9]
-		self.assertEqual(res, self.mm.Matrix.multiplication(self.mat,self.mat))
+                res = [[1,1,1],[1,4,9]]
+                self.assertEqual(res, mm.Matrix.multiplication(self.mat,self.mat,self.mat))
 
-	def test_sub_mat(self):
+        def test_sub_mat(self):
 	
-		res = [0,0,0][0,0,0]
-		self.assertEqual(res, self.mm.Matrix.multiplication(self.mat,self.mat))
+                res = [[0,0,0],[0,0,0]]
+                self.assertEqual(res, mm.Matrix.multiplication(self.mat,self.mat,self.mat))
 
 
 
